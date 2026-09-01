5e98ddbc9d09a7244ef348303e741f92b5acbc93
Nikolai Kopytov <nkopytov@itoolabs.com>
2026-09-01 11:32:25 +0300

    убрали синхронизацию за месяц

--- app/Console/Commands/SyncYcAllCommand.php
+++ app/Console/Commands/SyncYcAllCommand.php
@@ -27,8 +27,6 @@ final class SyncYcAllCommand extends Command
         'yclients:sync-records',
         'yclients:sync-transactions',
         'yclients:sync-storage-transactions',
-        'yclients:sync-company-month-stats',
-        'yclients:sync-staff-month-stats',
     ];
 
     /**
